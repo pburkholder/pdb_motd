8eea79c8680324023e0adafd1bd9595b97ece5d6
Peter Burkholder <peterb@getchef.com>
2014-11-13 14:45:14 -0500

    Rename to pdb_motd

--- test/unit/spec/default_spec.rb
+++ test/unit/spec/default_spec.rb
@@ -1,7 +1,7 @@
 # Encoding: utf-8
 require_relative 'spec_helper'
 
-describe 'motd::default' do
+describe 'pdb_motd::default' do
   let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
 
   it 'should create MOTD' do

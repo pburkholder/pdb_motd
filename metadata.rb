bbfd75c0cba0aee958ad4b19fbf3ecb9c4c970ff
Jenkins Continuous Integration Server <jenkins@ip-172-31-45-250.ec2.internal>
2015-05-19 20:20:24 +0000

    Bump version to v1.0.1

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@
 name                'pdb_motd'
 license             'Apache 2.0'
 description         'Example of testing'
-version             '1.0.0'
+version             '1.0.1'
 maintainer          'Peter Burkholder'
